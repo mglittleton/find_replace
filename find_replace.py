2f5363794fa15925ee5cfe8a855df1c89f6984c6
mglittleton <michaelglittleton@gmail.com>
2022-08-02 19:26:45 -0400

    finding the searchable strings inside the file

--- find_replace.py
+++ find_replace.py
@@ -6,12 +6,15 @@ import fileinput
 count = 0
 gen_file_obj = fileinput.input(files = sys.argv[1])
 text=''
+find = sys.argv[2]
+replace = sys.argv[3]
 
 # data manipulation
 for line in gen_file_obj:
   text += line
 
 # basic logic
+for i in range(0, len(text) - len(find) + 1):
+  print(text[i:i+len(find)])
 
 # output
-print (text)
